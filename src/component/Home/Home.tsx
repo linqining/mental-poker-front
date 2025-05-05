a9e8a0cf7fb3decf02c7e1ed5c5046bcbade3084
linqining <linqining@outlook.com>
2025-05-05 10:35:24 +0800

    userlogin button

--- src/component/Home/Home.tsx
+++ src/component/Home/Home.tsx
@@ -1,14 +1,31 @@
 import "../../css/Home.css";
-import { useCurrentAccount } from "@mysten/dapp-kit";
+import {ConnectButton, useCurrentAccount} from "@mysten/dapp-kit";
 
 const Home = () => {
   let account = useCurrentAccount();
   return (
-    <div className={"home-container"}>
-        <div >
-                {account ? <div className="guest-login"></div> : <div>Connect Wallet</div>}
-        </div>
-    </div>
+      <div className={"home-container"}>
+          {/*<div>*/}
+          {/*    {account ? <div>快速开始</div> : <div className="guest-login"></div>}*/}
+          {/*</div>*/}
+          <div>
+              {account ? <div>快速开始</div> :
+                  <ConnectButton
+                      connectText=""
+                      style={{
+                          backgroundColor: '#4CAF50',
+                          color: 'white',
+                          padding: '10px 15px',
+                          borderRadius: '5px',
+                          border: 'none',
+                          cursor: 'pointer'
+                      }}
+                      className="guest-login"
+                  />
+              }
+          </div>
+
+      </div>
   );
 };
 
