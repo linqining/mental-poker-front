94bb819f65e52839346e10541044be6b1691087c
linqining <linqining@outlook.com>
2025-05-22 05:05:43 +0800

    host addr

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@ import axios from 'axios' // 引入axios
 
 const service = axios.create({
   // baseURL: import.meta.env.VITE_BASE_API,
-  baseURL: "http://localhost:8989/",
+  baseURL: "http://43.198.209.189:8989/",
   timeout: 99999
 })
 
